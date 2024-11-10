f07539d187691e8734982a7f4d8ef78773b97984
AlexisHat <alexis.hatjiefthimiou@gmail.com>
2024-11-10 19:24:29 +0100

    MainController erstellt, welcher nur die Index Seite bisher zurück gibt und Test erstellt der übeprüft, ob die Request auf die Url erfolgereich ist

---
+++ src/main/java/de/propra/exam/coltrollers/MainController.java
@@ -0,0 +1,14 @@
+package de.propra.exam.coltrollers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+    
+}
---
+++ src/test/java/de/propra/exam/coltrollers/MainControllerTest.java
@@ -0,0 +1,26 @@
+package de.propra.exam.coltrollers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MainController.class)
+class MainControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    @DisplayName("Die Anwendung akzeptiert GET-req auf die Index seite")
+    public void test_01() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+}
