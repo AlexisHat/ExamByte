39ee937185d902b87fce8c9b2edb6fa5b03f958d
Philipp Fels <philippfels@hotmail.de>
2024-11-15 14:08:22 +0100

    add annotations for Roles

---
+++ src/main/java/de/propra/exam/config/annotations/KorrektorOnly.java
@@ -0,0 +1,14 @@
+package de.propra.exam.config.annotations;
+
+import org.springframework.security.access.annotation.Secured;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Secured("ROLE_KORREKTOR")
+public @interface KorrektorOnly {
+}
